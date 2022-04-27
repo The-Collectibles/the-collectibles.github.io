9aef55ae0c9bc5baefb4d6d6e98b4b75f2a8e639
Hemang Rajyaguru <hemdagem@gmail.com>
2022-04-27 11:14:50 +0100

    added listings for brands

--- src/templates/BrandPage.tsx
+++ src/templates/BrandPage.tsx
@@ -2,20 +2,28 @@ import * as React from "react";
 import { PageProps, graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 
-type Result = {
-  url: string;
-  name: string;
-  merchant: string;
-  linkToProduct: string;
-  productImage: string;
-  productGalleryImage: string;
-  description: string;
-  price: string;
-  brand: string;
+type data = {
+  allCustomApi: allCustomApi;
 };
-const BrandPage = (data: PageProps<Result, Result>) => {
-  console.log(data.pageContext.brand);
-  console.log(data.pageContext.brand);
+
+type allCustomApi = {
+  nodes: result[];
+};
+
+type result = {
+  brand: string;
+  sku: string;
+  description: string;
+  imageUrl: string;
+  stockMessage: string;
+  uid: string;
+  url: string;
+  thumbnailImageUrl: string;
+  price: string;
+  name: string;
+};
+const BrandPage = (data: PageProps<data, result>) => {
+
   return (
     <main>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -96,7 +104,7 @@ const BrandPage = (data: PageProps<Result, Result>) => {
           </div>
         </div>
       </nav>
-       <Helmet>
+      <Helmet>
         <title>{data.pageContext.brand} | The Collectibles</title>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
@@ -104,13 +112,29 @@ const BrandPage = (data: PageProps<Result, Result>) => {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
         />
-      </Helmet> 
+      </Helmet>
 
       <div className="container my-4">
         <div className="row">
-          <div className="col">
-           {data.pageContext.brand}
+          <div className="col"><h1>{data.pageContext.brand}</h1></div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {console.log(data.data.allCustomApi)}
+          {data.data.allCustomApi.nodes.map(item => (
+
+            <div className="col">
+            <div className="card w-75">
+              <img src={item.thumbnailImageUrl} className="img-fluid" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+              </div>
+            </div>
           </div>
+          
+          ))
+          }
+          
+
         </div>
       </div>
     </main>
@@ -118,17 +142,22 @@ const BrandPage = (data: PageProps<Result, Result>) => {
 };
 
 export const query = graphql`
-query MyQuery($brand : String) {
-  allCustomApi(filter: {brand: {eq: $brand}}) {
-    edges {
-      node {
-        id
+  query MyQuery($brand: String) {
+    allCustomApi(filter: { brand: { eq: $brand } }) {
+      nodes {
         brand
+        sku
+        description
+        imageUrl
+        stockMessage
+        uid
+        url
+        thumbnailImageUrl
+        price
         name
       }
     }
   }
-}
-`
+`;
 
 export default BrandPage;
